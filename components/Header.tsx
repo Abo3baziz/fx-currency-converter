9e0fa362859660f27f405e3405be0e51a54d6bc2
Ahmed Aziz <azez55.ali@gmail.com>
2026-06-21 20:55:44 +0300

    add header component  with logo icon

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Logo from "@/public/images/logo.svg";
+
+export default function Header() {
+  return (
+    <header className="p-200 flex justify-between items-center">
+      <Image
+        src={Logo}
+        alt="FX Logo"
+        className="max-mobile:w-25 w-37.5"
+        loading="eager"></Image>
+
+      <p className="text-header-text-color max-mobile:text-[10px]">
+        55 CURRENCIES · EOD · ECB DATA
+      </p>
+    </header>
+  );
+}
